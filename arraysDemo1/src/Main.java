5c0b875c1fe7304ab222c522418e603c720cf844
Oğuz Gürkan Altunok <oguzgurkan2001@gmail.com>
2021-07-05 22:39:41 +0300

    05.07.2021 Java Ders Sonu

---
+++ arraysDemo1/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    String[] students = new String[4];
+
+	    students[0] = "Gürkan";
+	    students[1] = "Engin";
+	    students[2] = "Derin";
+		students[3] = "Ahmet";
+		students[4] = "Ali";
+
+	    for (int i=0;i< students.length;i++){
+            System.out.println(students[i]);
+        }
+    }
+}
