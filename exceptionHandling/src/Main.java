7fe0c2e7cc00c0b77d95d0019a240dfb41be6ee3
gurkan <oguzgurkan2001@gmail.com>
2021-10-09 20:59:18 +0300

    09.10.2021

--- exceptionHandling/src/Main.java
+++ exceptionHandling/src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        String sehir = "Ankara";
-        int sayi = sehir;
+        try
+        {
+            int[] sayilar = new int[]{1, 2, 3};
+            System.out.println(sayilar[5]);
+        }catch (StringIndexOutOfBoundsException exception) {
+            System.out.println(exception);
+        }catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println(exception);
+        }catch (Exception exception){
+            System.out.println("Loglandı: "+exception);
+        }finally {
+            System.out.println("Ben her türlü çalışırım");
+        }
     }
 }
